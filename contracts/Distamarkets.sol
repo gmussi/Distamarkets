78ec802ac1a3d527a574b8e648937dd32182f488
Guilherme Mussi <gmussi@gmail.com>
2022-05-04 09:01:30 +0200

    add tests and new functions

--- contracts/Distamarkets.sol
+++ contracts/Distamarkets.sol
@@ -7,6 +7,7 @@ contract Distamarkets is Ownable {
     event MarketCreated(address indexed creator, uint256 indexed marketId, string name);
     event StakeAdded(uint256 indexed marketId, uint256 indexed outcomeId, uint256 amount, address indexed user);
     event StakeRemoved(uint256 indexed marketId, uint256 indexed outcomeId, uint256 amount, address indexed user);
+    event MarketStateChanged(uint256 indexed marketId, MarketState);
 
     enum MarketState { OPEN, CLOSED, CANCELED }
 
@@ -84,12 +85,14 @@ contract Distamarkets is Ownable {
         outcome.totalStake = outcome.totalStake - _amount;
         outcome.holders[msg.sender] = outcome.holders[msg.sender] - _amount;
 
+        payable(msg.sender).transfer(_amount);
+
         emit StakeRemoved(_marketId, _outcomeId, _amount, msg.sender);
 
         return outcome.holders[msg.sender];
     }
 
-    function getMarket(uint256 _marketId) public view returns (string memory, bytes32[] memory, MarketState) {
+    function getMarket(uint256 _marketId) public view returns (string memory, bytes32[] memory, MarketState, uint256) {
         Market storage market = markets[_marketId];
         
         bytes32[] memory outcomeNames = new bytes32[](market.numOutcomes);
@@ -97,7 +100,7 @@ contract Distamarkets is Ownable {
         for (uint i = 0; i < market.numOutcomes; i++) {
             outcomeNames[i] = (market.outcomes[i].outcomeName);
         }
-        return (market.title, outcomeNames, market.state);
+        return (market.title, outcomeNames, market.state, market.totalStake);
     }
 
     function getMarketIndex() public view returns (uint256) {
@@ -106,6 +109,10 @@ contract Distamarkets is Ownable {
 
     function getStake(address _holder, uint256 _marketId, uint256 _outcomeId) public view returns (uint256) {
         return markets[_marketId].outcomes[_outcomeId].holders[_holder];
+    }
+
+    function getMarketTotalStake(uint256 _marketId) public view returns (uint256) {
+        return markets[_marketId].totalStake;
     }
 
     modifier openMarket(uint256 _marketId) {
--- test/Distamarkets.js
+++ test/Distamarkets.js
@@ -37,6 +37,64 @@ describe("Distamarkets contract", () => {
             expect(outcomes[0]).to.equal(ethers.utils.formatBytes32String ('no'));
             expect(outcomes[1]).to.equal(ethers.utils.formatBytes32String ('yes'));
         });
+    });
 
+    describe("Betting", () => {
+        it ("Should allow adding stake", async () => {
+            // create market
+            await distamarkets.connect(addr1).createMarket("Will this first market work?", [ethers.utils.formatBytes32String ('no'), ethers.utils.formatBytes32String('yes')]);
+        
+            // add stake with both users
+            await distamarkets.connect(addr1).addStake(0, 0, {
+                value: ethers.utils.parseEther("50")
+            });
+            await distamarkets.connect(addr2).addStake(0, 1, {
+                value: ethers.utils.parseEther("25")
+            });
+
+            // checks stakes are counted correctly
+            let addr1Stake = await distamarkets.getStake(addr1.address, 0, 0);
+            expect(addr1Stake).to.equal(ethers.utils.parseEther("50"));
+
+            let addr2Stake = await distamarkets.getStake(addr2.address, 0, 1);
+            expect(addr2Stake).to.equal(ethers.utils.parseEther("25"));
+
+            // ensure no wrong stake
+            addr1Stake = await distamarkets.getStake(addr1.address, 0, 1);
+            expect(addr1Stake).to.equal("0");
+
+            // ensure stake is counted correctly
+            let totalStake = await distamarkets.getMarketTotalStake(0);
+            expect(totalStake).to.equal(ethers.utils.parseEther("75"));
+        });
+
+        it ("Should allow removing stake", async () => {
+            // create market
+            await distamarkets.connect(addr1).createMarket("Will this first market work?", [ethers.utils.formatBytes32String ('no'), ethers.utils.formatBytes32String('yes')]);
+            
+            // add stake
+            await distamarkets.connect(addr1).addStake(0, 0, {
+                value: ethers.utils.parseEther("50")
+            });
+
+            // track user current balance
+            let initialBalance = await addr1.getBalance();
+
+            // remove part of stake
+            await distamarkets.connect(addr1).removeStake(0, 0, ethers.utils.parseEther("10"));
+            
+            // ensure stake is tracked correctly
+            let addr1Stake = await distamarkets.getStake(addr1.address, 0, 0);
+            expect(addr1Stake).to.equal(ethers.utils.parseEther("40"));
+            
+            // ensure user received funds
+            let finalBalance = await addr1.getBalance();
+            console.log(initialBalance, finalBalance, finalBalance.sub(initialBalance));
+
+            expect(finalBalance.sub(initialBalance)).to.be.at.least(ethers.utils.parseEther("9"));
+            
+            
+             ;
+        });
     });
 });
