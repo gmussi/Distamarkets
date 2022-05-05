6d9170c9098616202b20606df59044745014f93a
Guilherme Mussi <gmussi@gmail.com>
2022-05-05 09:05:56 +0200

    deploy script also verifies the contract on etherscan

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
 async function main() {
+    
     const [deployer] = await ethers.getSigners();
     console.log(`Deploying contract using address ${deployer.address}`);
 
@@ -8,6 +9,16 @@ async function main() {
     const Distamarkets = await ethers.getContractFactory("Distamarkets");
     const distamarkets = await Distamarkets.deploy();
     console.log(`Distamarkets deployed at ${distamarkets.address}`);
+
+    console.log("Waiting 2 block confirmations...")
+    await distamarkets.deployTransaction.wait(6);
+
+    console.log("Verifying contract on etherscan.")
+    await hre.run("verify:verify", {
+        address: distamarkets.address
+    });
+      
+
 };
 
 main()
@@ -17,4 +28,4 @@ main()
     .catch(err => {
         console.error(err);
         process.exit(1);
-    })
+    });
