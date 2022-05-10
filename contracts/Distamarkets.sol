83ca0f4b1b378f7aa6c2f083273abc7b0952bae5
Guilherme Mussi <gmussi@gmail.com>
2022-05-10 10:09:32 +0200

    improve code styling

--- contracts/Distamarkets.sol
+++ contracts/Distamarkets.sol
@@ -38,42 +38,42 @@ contract Distamarkets {
         address user;
     }
 
-    IERC20 internal immutable token;
-    Market[] markets;
-    UserStake[] userStakes;
-    mapping(address => uint256[]) stakesByUser; // User => UserStake Ids
+    IERC20 internal immutable _token;
+    Market[] _markets;
+    UserStake[] _userStakes;
+    mapping(address => uint256[]) _stakesByUser; // User => UserStake Ids
 
-    constructor(IERC20 _token) {
-        token = _token;
+    constructor(IERC20 token_) {
+        _token = token_;
     }
 
-    function createMarket(string calldata _title, string calldata _image, bytes32[] memory _outcomeNames) external returns(uint256) {
-        markets.push();
-        uint marketId = markets.length;
+    function createMarket(string calldata title_, string calldata image_, bytes32[] memory outcomeNames_) external returns(uint256) {
+        _markets.push();
+        uint marketId = _markets.length;
         
-        Market storage market = markets[marketId - 1];
-        market.title = _title;
-        market.image = _image;
-        market.numOutcomes = _outcomeNames.length;
+        Market storage market = _markets[marketId - 1];
+        market.title = title_;
+        market.image = image_;
+        market.numOutcomes = outcomeNames_.length;
         market.creator = msg.sender;
         market.state = MarketState.OPEN;
 
-        for (uint i = 0; i < _outcomeNames.length; i++) {
+        for (uint i = 0; i < outcomeNames_.length; i++) {
             MarketOutcome storage outcome = market.outcomes[i];
             outcome.id = i;
-            outcome.outcomeName = _outcomeNames[i];
+            outcome.outcomeName = outcomeNames_[i];
         }
 
-        emit MarketCreated(msg.sender, marketId, _title); 
+        emit MarketCreated(msg.sender, marketId, title_); 
 
         return marketId;
     }
 
-    function addStake(uint256 _marketId, uint256 _outcomeId) external payable openMarket(_marketId) returns (uint256) {
+    function addStake(uint256 marketId_, uint256 outcomeId_) external payable openMarket(marketId_) returns (uint256) {
         require(msg.value > 0, "Cannot add 0 stake");
 
-        Market storage market = markets[_marketId - 1];
-        MarketOutcome storage outcome = market.outcomes[_outcomeId];
+        Market storage market = _markets[marketId_ - 1];
+        MarketOutcome storage outcome = market.outcomes[outcomeId_];
 
         market.totalStake = market.totalStake + msg.value;
         outcome.totalStake = outcome.totalStake + msg.value;
@@ -84,50 +84,50 @@ contract Distamarkets {
 
         if (stakeId == 0) {
             // stake does not exist yet
-            userStakes.push();
-            stakeId = userStakes.length;
-            stake = userStakes[stakeId - 1];
-            stake.marketId = _marketId;
-            stake.outcomeId = _outcomeId;
+            _userStakes.push();
+            stakeId = _userStakes.length;
+            stake = _userStakes[stakeId - 1];
+            stake.marketId = marketId_;
+            stake.outcomeId = outcomeId_;
 
-            stakesByUser[msg.sender].push(stakeId);
+            _stakesByUser[msg.sender].push(stakeId);
             outcome.holders[msg.sender] = stakeId;
         } else {
             // loading existing stake
-            stake = userStakes[stakeId - 1];
+            stake = _userStakes[stakeId - 1];
         }
         
         // update stake amount
         stake.amount = stake.amount + msg.value;
 
-        emit StakeChanged(stakeId, msg.value, _marketId, msg.sender);
+        emit StakeChanged(stakeId, msg.value, marketId_, msg.sender);
         
         return stake.amount;
     }
 
-    function removeStake(uint256 _stakeId, uint256 _amount) external payable openMarket(userStakes[_stakeId - 1].marketId) returns (uint256) {
-        require(_amount > 0, "Cannot remove 0 stake");
+    function removeStake(uint256 stakeId_, uint256 amount_) external payable openMarket(_userStakes[stakeId_ - 1].marketId) returns (uint256) {
+        require(amount_ > 0, "Cannot remove 0 stake");
 
-        UserStake storage stake = userStakes[_stakeId - 1];
+        UserStake storage stake = _userStakes[stakeId_ - 1];
 
-        Market storage market = markets[stake.marketId - 1];
+        Market storage market = _markets[stake.marketId - 1];
         MarketOutcome storage outcome = market.outcomes[stake.outcomeId];
 
-        require(stake.amount >= _amount, "Amount exceeds current stake");
+        require(stake.amount >= amount_, "Amount exceeds current stake");
 
-        market.totalStake = market.totalStake - _amount;
-        outcome.totalStake = outcome.totalStake - _amount;
-        stake.amount = stake.amount - _amount;
+        market.totalStake = market.totalStake - amount_;
+        outcome.totalStake = outcome.totalStake - amount_;
+        stake.amount = stake.amount - amount_;
 
-        payable(msg.sender).transfer(_amount);
+        payable(msg.sender).transfer(amount_);
 
-        emit StakeChanged(_stakeId, msg.value, stake.marketId, msg.sender);
+        emit StakeChanged(stakeId_, msg.value, stake.marketId, msg.sender);
 
         return stake.amount;
     }
 
-    function getMarket(uint256 _marketId) public view returns (string memory, string memory, MarketState, uint256, bytes32[] memory, uint256[] memory) {
-        Market storage market = markets[_marketId - 1];
+    function getMarket(uint256 marketId_) public view returns (string memory, string memory, MarketState, uint256, bytes32[] memory, uint256[] memory) {
+        Market storage market = _markets[marketId_ - 1];
         
         bytes32[] memory outcomeNames = new bytes32[](market.numOutcomes);
         uint256[] memory outcomeStakes = new uint256[](market.numOutcomes);
@@ -141,31 +141,31 @@ contract Distamarkets {
     }
 
     function getMarketIndex() public view returns (uint256) {
-        return markets.length;
+        return _markets.length;
     }
 
-    function getStakeId(address _holder, uint256 _marketId, uint256 _outcomeId) public view returns (uint256) {
-        return markets[_marketId - 1].outcomes[_outcomeId].holders[_holder];
+    function getStakeId(address holder_, uint256 marketId_, uint256 outcomeId_) public view returns (uint256) {
+        return _markets[marketId_ - 1].outcomes[outcomeId_].holders[holder_];
     }
 
-    function getMarketTotalStake(uint256 _marketId) public view returns (uint256) {
-        return markets[_marketId - 1].totalStake;
+    function getMarketTotalStake(uint256 marketId_) public view returns (uint256) {
+        return _markets[marketId_ - 1].totalStake;
     }
 
-    function getUserStakes(address _address) public view returns(uint256[] memory) {
-        return stakesByUser[_address];
+    function getUserStakes(address address_) public view returns(uint256[] memory) {
+        return _stakesByUser[address_];
     }
 
-    function getStake(uint256 stakeId) public view returns(UserStake memory) {
-        return userStakes[stakeId - 1];
+    function getStake(uint256 stakeId_) public view returns(UserStake memory) {
+        return _userStakes[stakeId_ - 1];
     }
 
-    function getToken() public view returns (IERC20) {
-        return token;
+    function token() public view returns (IERC20) {
+        return _token;
     }
 
-    modifier openMarket(uint256 _marketId) {
-        require(markets[_marketId - 1].state == MarketState.OPEN);
+    modifier openMarket(uint256 marketId_) {
+        require(_markets[marketId_ - 1].state == MarketState.OPEN);
         _;
     }
 }
